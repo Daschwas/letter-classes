b825b8a7acbdd2df893eb4a1d2de37308156c646
Daschwas <schwass.dale@gmail.com>
2023-09-15 23:30:04 +0800

    setting up test branch for part 3

--- AT2Task2/town.py
+++ AT2Task2/town.py
@@ -2,8 +2,10 @@ class Town:
     def __init__(self, title):
         self.title = title
         self.residents = []
+        self.post_offices = []
 
     def add_residents(self, resident):
         self.residents.append(resident)
-
+    def add_post_offices(self, post_office):
+        self.post_office.append(post_office)
 
